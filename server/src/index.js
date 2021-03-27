38282a3f11f34fcde296b434de0aee6c56ab6ae4
Vlad Boichenko <v.boichenko@4irelabs.com>
2021-03-27 02:36:04 +0200

    feat: added auth route

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+// import './models';
+// import { auth } from './routes';
+
+const app = express();
+app.use(express.urlencoded());
+
+app.use(express.json());
+// app.use('/api/v1/', auth);
+const appPort = 3000;
+app.listen(
+    appPort,
+    () => console.log(`Listening on port ${appPort}...`),
+);
---
+++ server/src/routes/auth.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+const studentRegistration = async (req, res) => {
+
+};
+router.post('/student', studentRegistration);
+
+export default router;
