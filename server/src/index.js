d635107e290c76b628dee67b02c0eb9ba7b83980
Olexandra Chumachenko <a.r.chumachenko@gmail.com>
2021-04-02 18:29:59 +0300

    Add teacher and user validation

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,11 @@
 import express from 'express'
-// import './models';
-// import { auth } from './routes';
+import { auth } from './routes';
 
 const app = express();
 app.use(express.urlencoded());
 
 app.use(express.json());
-// app.use('/api/v1/', auth);
+app.use('/api/v1/', auth);
 const appPort = 3000;
 app.listen(
     appPort,
